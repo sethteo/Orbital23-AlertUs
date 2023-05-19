139b20c6871d5d45857929e9d500724c8e80890a
Teo Hong Rui <119569146+sethteo@users.noreply.github.com>
2023-05-20 01:26:38 +0800

    Update bot.py
    Updated current telegram bot with working inline buttons.

--- bot.py
+++ bot.py
@@ -1,59 +1,54 @@
 import logging
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import (
-    filters,
-    MessageHandler,
-    ApplicationBuilder,
-    CommandHandler,
-    ContextTypes,
-)
 
-# Our bot token
-BOT_TOKEN = "5660209243:AAFa6yf8AuxLLq2spli4NTjTLj03lGKA1_Q"
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+API_TOKEN = '5660209243:AAFa6yf8AuxLLq2spli4NTjTLj03lGKA1_Q'
 WELCOME_TEXT = "Hi I am the AlertUs Bot and can help you to track the prices of your items." \
                " Please select one of the options to continue"
-ERROR_TEXT = "Sorry, I did not understand that. Please try again."
 
-# Setting up our logger
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+# Configure logging
+logging.basicConfig(level=logging.INFO)
 
-
-# Functions to handle each command
-# def start(update, context):
-#     context.bot.send_message(chat_id=update.effective_chat.id, text=WELCOME_TEXT)
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=WELCOME_TEXT)
+button_1 = InlineKeyboardButton(text="Shopee", callback_data="1")
+button_2 = InlineKeyboardButton(text="Lazada", callback_data="2")
+keyboard = InlineKeyboardMarkup().add(button_1, button_2)
 
 
-# async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    """
+    This handler will be called when user sends `/start` command
+    """
+    await message.reply(WELCOME_TEXT)
 
 
-async def caps(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    text_caps = ' '.join(context.args).upper()
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+@dp.message_handler(commands=['begin'])
+async def begin(message: types.Message):
+    await message.reply("Select an option", reply_markup=keyboard)
 
 
-async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=ERROR_TEXT)
+@dp.callback_query_handler(text=["1", "2"])
+async def function(call: types.callback_query):
+    if call.data == "1":
+        await call.message.answer(foo1())
+    if call.data == "2":
+        await call.message.answer(foo2())
+    await call.answer()
+
+
+def foo1():
+    return "Shopee"
+
+
+def foo2():
+    return "Lazada"
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(BOT_TOKEN).build()
-
-    start_handler = CommandHandler('start', start)
-    #echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    caps_handler = CommandHandler('caps', caps)
-    unknown_handler = MessageHandler(filters.COMMAND, unknown)
-
-    application.add_handler(start_handler)
-    #application.add_handler(echo_handler)
-    application.add_handler(caps_handler)
-    application.add_handler(unknown_handler)
-
-    application.run_polling()
-
-
+    executor.start_polling(dp, skip_updates=True)
