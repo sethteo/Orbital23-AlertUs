18f285fc1d681c15e4ba44a92145366444eb1d49
Teo Hong Rui <119569146+sethteo@users.noreply.github.com>
2023-05-22 23:02:08 +0800

    Added implementation of database methods
    Included implementation of the logic to create new users add their item and checking if the user exists

--- scraper.py
+++ scraper.py
@@ -1,33 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+from database import check_exist, create_new_user, add_item
 
 
-def scrape_ntuc(url):
+# Scraping function that handles url from NTUC
+def scrape_ntuc(url, username):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     price_element = soup.find("span", class_="sc-1bsd7ul-1 sc-13n2dsm-5 fQJDmX cpRMKK")
     price = price_element.text.strip()
+    add_to_db(url, price, username)
+
     print(price)
     return price
 
 
-def scrape_cs(url):
+# Scraping function that handles url from Cold Storage
+def scrape_cs(url, username):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     price_element = soup.find("div", class_="product-detail")
     price_data = price_element.find("div", class_="price_now price_normal f-green disc")
     price = price_data.text.strip()
+    add_to_db(url, price, username)
+
     print(price)
     return price
 
 
-def scrape_ss(url):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    price_element = soup.find("div", class_="product-price-tag")
+# Helper method to facilitate user creation and user update
+def add_to_db(url, price, username):
+    if check_exist(username):
+        add_item(url, price, username)
+    else:
+        create_new_user(username)
+        add_item(url, price, username)
 
-    return price_element
-
-# scrape_ntuc("https://www.fairprice.com.sg/product/aw-s-market-fresh-malaysian-pork-big-spare-ribs-300-g-90110551")
-# scrape_cs("https://coldstorage.com.sg/meadows-roasted-cashews-150g-5071784")
-scrape_ss("https://shengsiong.com.sg/product/kelloggs-soy-protein-granola-honey-almond-220-gram")
