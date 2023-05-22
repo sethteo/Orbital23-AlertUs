69a89591ee207297d79013901fcaed4378eade5f
Teo Hong Rui <119569146+sethteo@users.noreply.github.com>
2023-05-22 23:23:17 +0800

    Added item name scraping functionality
    added code that helps to retrieve the item name of the user's item

--- scraper.py
+++ scraper.py
@@ -8,10 +8,12 @@ def scrape_ntuc(url, username):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     price_element = soup.find("span", class_="sc-1bsd7ul-1 sc-13n2dsm-5 fQJDmX cpRMKK")
+    item_element = soup.find("span", class_="sc-1bsd7ul-1 beGvbz")
     price = price_element.text.strip()
-    add_to_db(url, price, username)
+    item_name = item_element.text.strip()
+    add_to_db(url, price, item_name, username)
 
-    print(price)
+    print(price, item_name)
     return price
 
 
@@ -19,20 +21,25 @@ def scrape_ntuc(url, username):
 def scrape_cs(url, username):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    price_element = soup.find("div", class_="product-detail")
-    price_data = price_element.find("div", class_="price_now price_normal f-green disc")
+    product = soup.find("div", class_="product-detail")
+    price_data = product.find("div", class_="price_now price_normal f-green disc")
+    name_data = product.find("h1", class_="product-name main-heading")
     price = price_data.text.strip()
-    add_to_db(url, price, username)
+    item_name = name_data.text.strip()
+    add_to_db(url, price, item_name, username)
 
-    print(price)
+    print(price, item_name)
     return price
 
 
 # Helper method to facilitate user creation and user update
-def add_to_db(url, price, username):
+def add_to_db(url, price, item_name, username):
     if check_exist(username):
-        add_item(url, price, username)
+        add_item(url, price, item_name, username)
     else:
         create_new_user(username)
-        add_item(url, price, username)
+        add_item(url, price, item_name, username)
 
+
+scrape_ntuc("https://www.fairprice.com.sg/product/pasar-prepacked-carrots-500g-13000321", "hr")
+scrape_cs("https://coldstorage.com.sg/cabernet-sauvignon-5007180", "hr")
