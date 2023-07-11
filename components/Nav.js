65c5f17e3691ca41346e7dd22499d3a0e08d99b7
slimzaza <106515785+slimzaza@users.noreply.github.com>
2023-07-11 13:20:02 +0800

    Created initial home and item page

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,19 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Nav() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
-                        <Link to='/' className="nav-item nav-link active">Home</Link>
-                        <Link to='/tweets' className="nav-item nav-link">Items</Link>
-                    </div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark top">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="navMainMenu" class="navbar-collapse collapse">
+                <div class="navbar-nav ml-auto">
+                    <Link to='/' className="nav-item nav-link active">Home</Link>
+                    <Link to='/items' className="nav-item nav-link">Tweets</Link>
                 </div>
             </div>
         </nav>
     )
 }
-
 export default Nav;
